5ef4f8768f04888deee470966b9c2c5d19f56ef5
Erik <erikadarisman@gmail.com>
2020-02-22 15:14:57 +0700

    first API get Category

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/Erikadarisman/go-rest-api/driver"
+	"github.com/Erikadarisman/go-rest-api/models"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -17,6 +22,40 @@ func init() {
 
 func main() {
 	db = driver.ConnectDB()
+	defer db.Close()
+	router := mux.NewRouter()
+	router.HandleFunc("/getcategories", getAllCategory).Methods("GET")
+	http.Handle("/", router)
+	fmt.Println("Connected to port 9999")
+	log.Fatal(http.ListenAndServe(":9999", router))
 
-	log.Println("Server started successfully on port 8000.!!!")
+	// log.Println("Server started successfully on port 8000.!!!")
+}
+
+func getAllCategory(w http.ResponseWriter, r *http.Request) {
+
+	var category models.Category
+	var arrCat []models.Category
+	var response models.Response
+
+	rows, err := db.Query("Select id,name from public.category")
+	if err != nil {
+		log.Print(err)
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&category.ID, &category.Name); err != nil {
+			log.Fatal(err.Error())
+
+		} else {
+			arrCat = append(arrCat, category)
+		}
+	}
+
+	response.Status = 200
+	response.Message = "Success"
+	response.Data = arrCat
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
